5aa97612adc7a105ada3814b282fca60b37d558e
ThieuNv <nguyenthieu2102@gmail.com>
2017-11-30 11:41:30 +0700

    Dev, ThieuNv, update rest for chart

---
+++ src/main/java/com/project/tuyensinhdaihoc/web_layer/controller/client/rest/RestChartController.java
@@ -0,0 +1,33 @@
+package com.project.tuyensinhdaihoc.web_layer.controller.client.rest;
+
+import com.project.tuyensinhdaihoc.service_layer.UniversalPointService;
+import com.project.tuyensinhdaihoc.web_layer.dto.UniversalPointVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest")
+public class RestChartController {
+
+    private final UniversalPointService universalPointService;
+
+    @Autowired
+    public RestChartController(UniversalPointService universalPointService) {
+        this.universalPointService = universalPointService;
+    }
+
+    @RequestMapping(value = "/chart",method = RequestMethod.GET)
+    public List<UniversalPointVO> getListChartById(@RequestParam Integer id){
+        return universalPointService.findUniversalPointById(id);
+    }
+
+    @RequestMapping(value = "/chart",method = RequestMethod.GET)
+    public List<UniversalPointVO> getListChartByBlockName(@RequestParam String blockName){
+        return universalPointService.findUniversalPointByBlockName(blockName);
+    }
+}
